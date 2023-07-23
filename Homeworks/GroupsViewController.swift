4aa6155afe6576ab601fa0d49c88ac8c523700d5
Ivan <i.balabai@gmail.com>
2023-07-23 23:31:09 +0300

    Photocell has been added

--- Homeworks/GroupsViewController.swift
+++ Homeworks/GroupsViewController.swift
@@ -36,15 +36,15 @@ class GroupsViewController: UITableViewController {
         return 6
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+      //  let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
         // Configure the cell...
 
-        return cell
+        return GroupCell()
     }
-    */
+    	
 
     /*
     // Override to support conditional editing of the table view.
