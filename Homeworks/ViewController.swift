e0020a583c7a8b2b0c6cfd712eab4137611ad114
Ivan <i.balabai@gmail.com>
2023-09-19 00:11:06 +0300

    working with task3 main.md added

--- Homeworks/ViewController.swift
+++ Homeworks/ViewController.swift
@@ -7,14 +7,28 @@
 // This is working project
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView(frame: 	view.bounds)
+        webView.navigationDelegate = self
+        return webView
+            } ()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setupViews()
+        
+        let url = URL(string: "https://oauth.vk.com/authorize?client_id=" + AppCode.appCode +
+        "&redirect_uri=https://oauth.vk.com/blank.html&scope=262150&display=mobile&response_type=token")
+        webView.load(URLRequest(url: url!))
     }
-
+    private func setupViews() {
+        view.addSubview(webView)
+    }
 
 }
 
