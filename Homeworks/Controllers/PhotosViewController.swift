dff9c77576c70ade9aae6c64d8f6e2d9732254cb
Ivan <i.balabai@gmail.com>
2023-12-05 17:55:56 +0300

    Fixes to photosviewcontroller;

--- Homeworks/Controllers/PhotosViewController.swift
+++ Homeworks/Controllers/PhotosViewController.swift
@@ -15,6 +15,8 @@ class PhotosViewController:UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = Theme.currentTheme.backgroundColor
+        collectionView.backgroundColor = Theme.currentTheme.backgroundColor
         title = "Photos"
         collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: "Photos")
         networkService.getPhotos{[weak self] photos in
@@ -46,44 +48,3 @@ extension PhotosViewController: UICollectionViewDelegateFlowLayout {
         return CGSize(width: cellSize, height: cellSize)
     }
 }
-
-
-
-//private let reuseIdentifier = "Cell"
-
-//final class PhotosViewController: UICollectionViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        title = "Photos"
-//        collectionView.delegate = self
-//        collectionView.dataSource = self
-////        if let layout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
-////            layout.delegate = self}
-//        collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: Constants.Identifier.photoCellIdentifier)
-//        }
-//
-//
-//
-//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 6
-//    }
-//
-//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.Identifier.photoCellIdentifier, for: indexPath) as! PhotoCell
-//        return cell
-//    }
-//}
-//
-//    extension PhotosViewController: UICollectionViewDelegateFlowLayout {
-//            func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//            let width = UIScreen.main.bounds.width
-//            let cellSize = width / 2 - 20
-//            return CGSize(width: cellSize, height: cellSize)
-//
-//        }
-//
-//    }
-
-    
